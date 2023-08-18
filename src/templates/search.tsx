90a31cecd2ef20e97a0f0df77bd107f2fc3aa02c
Erik Summerfield <e2thex@eriksummerfield.lan>
2023-08-17 22:59:50 -0500

    Change made based on module

--- src/templates/search.tsx
+++ src/templates/search.tsx
@@ -8,9 +8,24 @@ import {
   TemplateProps,
 } from "@yext/pages";
 import "../index.css";
+import {
+  SearchHeadlessProvider,
+  provideHeadless,
+  HeadlessConfig,
+  Environment,
+} from "@yext/search-headless-react";
+
+import {
+  SearchBar,
+  StandardCard,
+  VerticalResults,
+  SpellCheck,
+  ResultsCount,
+  Pagination,
+  } from "@yext/search-ui-react";
 
 export const getPath: GetPath<TemplateProps> = () => {
-  return "search";
+  return "/search";
 };
 
 export const getHeadConfig: GetHeadConfig<
@@ -23,8 +38,33 @@ export const getHeadConfig: GetHeadConfig<
   };
 };
 
+const headlessConfig: HeadlessConfig = {
+  apiKey: "416765780fdc2d0c1daab985a0ccc178",
+  experienceKey: "turtlehead",
+  locale: "en",
+  verticalKey: "faqs",
+  environment: Environment.SANDBOX,
+}
+
+const searcher = provideHeadless(headlessConfig);
+
 const Search: Template<TemplateRenderProps> = () => {
-  return <>Your Code Here!</>;
+  return (
+    <SearchHeadlessProvider searcher={searcher}>
+      <div className="px-4 py-8">
+        <div className="mx-auto flex max-w-5xl flex-col">
+          <SearchBar />
+          <SpellCheck />
+          <ResultsCount />
+          <VerticalResults
+            CardComponent={StandardCard}
+            displayAllOnNoResults={false}
+          />
+        </div>
+        <Pagination />
+      </div>
+    </SearchHeadlessProvider>
+  );
 };
 
 export default Search;
